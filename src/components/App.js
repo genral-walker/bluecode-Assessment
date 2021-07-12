0c6f25a254ff707a29de382824f6ae0be58313c2
Lukman Sanni <lukmansanni60@gmail.com>
2021-07-12 11:34:57 +0100

    Finish aside section without responsivesness.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+import './App.scss';
+import Navigation from './Navigation/Navigation';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navigation />
+
+      <main className='main'>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,68 @@
+
+import React from 'react';
+import styles from './Navigation.module.scss'
+
+import { ReactComponent as Cube } from '../../assets/svgs/cube.svg';
+import { ReactComponent as Home } from '../../assets/svgs/home.svg';
+import { ReactComponent as Message } from '../../assets/svgs/message.svg';
+import { ReactComponent as Rank } from '../../assets/svgs/ranking.svg';
+import { ReactComponent as Challenge } from '../../assets/svgs/challenge.svg';
+import { ReactComponent as Party } from '../../assets/svgs/party.svg';
+import { ReactComponent as Connect } from '../../assets/svgs/connect.svg';
+import { ReactComponent as Parade } from '../../assets/svgs/parade.svg';
+import { ReactComponent as Group } from '../../assets/svgs/group.svg';
+
+
+const Navigation = () => {
+
+    return (
+        <aside className={styles.body}>
+            <div className={styles.header}>
+                <Cube />
+                <h1>bluecube</h1>
+            </div>
+
+            <button className={styles.home}>
+                <Home /> <span>Home</span>
+            </button>
+
+            <button>
+                <Message />
+                <span>Message</span>
+            </button>
+
+            <p className={styles.share}>SHARE</p>
+
+            <button>
+                <Rank />
+                <span>Ranking</span>
+            </button>
+
+            <button>
+                <Challenge />
+                <span>Challenge</span>
+            </button>
+
+            <button>
+                <Party />
+                <span>Party</span>
+            </button>
+
+            <button>
+                <Connect />
+                <span>Connect</span>
+            </button>
+
+            <button>
+                <Parade />
+                <span>Parade</span>
+            </button>
+
+            <button>
+                <Group />
+                <span>Group</span>
+            </button>
+        </aside>
+    )
+};
+export default Navigation;
